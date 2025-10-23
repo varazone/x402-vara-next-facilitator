7f07c2bec2a9f20cd99805713ca88a029851cb76
Broken Pipe <navigaid@gmail.com>
2025-10-23 15:28:31 +0800

    migrate middleware to proxy
    https://nextjs.org/docs/messages/middleware-to-proxy

--- proxy.ts
+++ proxy.ts
@@ -1,6 +1,6 @@
 import { NextRequest, NextResponse } from 'next/server';
 
-export function middleware(req: NextRequest) {
+export function proxy(req: NextRequest) {
   console.log(`[Middleware] ${req.method} ${req.nextUrl.pathname}`);
   // You must return something — usually the request continues as-is:
   return NextResponse.next();
